319d9c809cf4d1abceb07e4447627f26bcce7440
cisar2218 <cisar2218@gmail.com>
2025-09-30 12:21:35 +0200

    WIP contract state reading

--- PassetHub/InkSharp/PoC.cs
+++ PassetHub/InkSharp/PoC.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Diagnostics;
 using System.Security.Cryptography;
 using PassetHub.NetApi.Generated.Model.frame_system;
+using PassetHub.NetApi.Generated.Model.pallet_revive.storage;
+using PassetHub.NetApi.Generated.Model.primitive_types;
 using PassetHub.NetApi.Generated.Types.Base;
 using Substrate.NetApi;
 using Substrate.NetApi.Model.Extrinsics;
@@ -11,7 +14,7 @@ namespace InkSharp;
 
 public class PoC
 {
-    public static async Task<AccountInfo> SamplePassetHubRpcCall()
+    public static async Task<PassetHub.NetApi.Generated.Model.frame_system.AccountInfo> SamplePassetHubRpcCall()
     {
         var token = CancellationToken.None;
 
@@ -28,6 +31,28 @@ public class PoC
         // Query chain state
         var result = await substrateClient.SystemStorage.Account(
             account,
+            null,
+            token
+        );
+
+        return result;
+    }
+
+    public static async Task<PassetHub.NetApi.Generated.Model.pallet_revive.storage.AccountInfo> QueryContractStorage_AccountInfoAtAddress(string contractAddress)
+    {
+        var token = CancellationToken.None;
+
+        var substrateClient = new PassetHub.NetApi.Generated.SubstrateClientExt(new Uri("wss://asset-hub-paseo.dotters.network"), ChargeTransactionPayment.Default());
+        await substrateClient.ConnectAsync();
+
+        H160 contract = new H160();
+        contract.Create(contractAddress);
+
+        Debug.Assert(contract.Bytes.Length == 20, "Contract address probably wrong format.");
+        
+        // Query chain state
+        var result = await substrateClient.ReviveStorage.AccountInfoOf(
+            contract,
             null,
             token
         );
--- PassetHub/InkSharpLayer.Tests/UnitTestPocClass.cs
+++ PassetHub/InkSharpLayer.Tests/UnitTestPocClass.cs
@@ -1,6 +1,9 @@
 ﻿using InkSharp;
 using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using Newtonsoft.Json.Linq;
 using PassetHub.NetApi.Generated.Model.frame_system;
+using PassetHub.NetApi.Generated.Model.pallet_revive.storage;
+using Substrate.NetApi;
 using Xunit.Abstractions;
 
 namespace InkSharpLayer.Tests
@@ -19,9 +22,29 @@ namespace InkSharpLayer.Tests
         [Fact]
         public async Task TestPassetHubRpcOk()
         {
-            AccountInfo result = await PoC.SamplePassetHubRpcCall();
+            PassetHub.NetApi.Generated.Model.frame_system.AccountInfo result = await PoC.SamplePassetHubRpcCall();
 
             _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public async Task TestDaoContractInfoQueryOk()
+        {
+            string contractAddress = "0x170eAb77A1911b43cE5B9E09fd610861026AD42e";
+            PassetHub.NetApi.Generated.Model.pallet_revive.storage.AccountInfo result = await PoC.QueryContractStorage_AccountInfoAtAddress(contractAddress);
+
+            Assert.NotNull(result);
+            Assert.Equal(expected: AccountType.Contract, result.AccountType.Value);
+            _output.WriteLine(result.ToString());
+        }
+
+        [Fact]
+        public async Task TestDecodeOfResponse()
+        {
+            PassetHub.NetApi.Generated.Model.pallet_revive.storage.AccountInfo info = new PassetHub.NetApi.Generated.Model.pallet_revive.storage.AccountInfo();
+            info.Create("0x735f040a5d490f1107ad9c56f5ca00d2ae37ff0591fdbbcd9c2406df7147a9dc170eab77a1911b43ce5b9e09fd610861026ad42e");
+
+            _output.WriteLine(info.ToString());
         }
     }
 }
